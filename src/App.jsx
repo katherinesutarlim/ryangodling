567dbf165a50e34b539ca9f02116fc353e853033
astridbina <astridprabowoutami@yahoo.com>
2023-12-08 21:15:17 +1100

    added linting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React from 'react'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
