d9f5e38467a091c655f39667eee30b36a82efb33
Tung Quan Hoang <hoangtungquan22@gmail.com>
2023-12-09 12:16:23 +1100

    revert change to mainpage to avoid conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import './App.css'
 import React from 'react'
 import Footer from './components/Footer'
 import Homepage from './components/Homepage'
-import CopypastaForm from './components/CopypastaForm'
 
 function App () {
   return (
     <>
     <Homepage>
     </Homepage>
-    <CopypastaForm></CopypastaForm>
     <Footer>
     </Footer>
     </>
