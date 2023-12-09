ec7d0d0b805d7de8ab528174712fe07f9cc8d85e
Tung Quan Hoang <hoangtungquan22@gmail.com>
2023-12-09 15:44:41 +1100

    change the button to ryan gosling images and remove a semicolon in app.jsx

--- src/components/Rickroll.jsx
+++ src/components/Rickroll.jsx
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import './Rickroll.css'
 
 const Rickroll = () => {
   const totalLinks = 12
   const [correctLink, setCorrectLink] = useState(null)
+
+  useEffect(() => {
+    const elements = document.querySelectorAll('.link-button')
+
+    elements.forEach(el => {
+      const x = Math.random() * window.innerWidth
+      const y = Math.random() * window.innerHeight
+      el.style.position = 'absolute'
+      el.style.left = `${x}px`
+      el.style.top = `${y}px`
+    })
+  }, [])
 
   useEffect(() => {
     setCorrectLink(Math.floor(Math.random() * totalLinks))
@@ -19,8 +32,7 @@ const Rickroll = () => {
   return (
         <div>
             {[...Array(totalLinks).keys()].map(linkId => (
-                <button key={linkId} onClick={() => handleLinkClick(linkId)}>
-                    Link {linkId + 1}
+                <button className='link-button' key={linkId} onClick={() => handleLinkClick(linkId)}>
                 </button>
             ))}
         </div>
